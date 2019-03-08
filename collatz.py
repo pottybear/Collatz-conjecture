c04d98afd141b344a668030b1b3dcb88b367c1da
PARK JOON GYU <vpark45@gmail.com>
2019-03-08 19:23:39 +0900

    Create collatz.py

---
+++ collatz.py
@@ -0,0 +1,20 @@
+def even(n):
+    return n % 2 == 0
+
+
+def odd(n):
+    return not even(n)
+
+
+def collatz(n):
+    if n == 1:
+        return [1]
+    elif even(n):
+        return [n] + collatz(n // 2)
+    elif odd(n):
+        return [n] + collatz(n*3 + 1)
+
+
+if __name__ == '__main__':
+    n = int(input())
+    print(collatz(n))
