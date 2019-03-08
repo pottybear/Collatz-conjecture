0da33ee0d0eee8c7078027846c477499de28039f
PARK JOON GYU <vpark45@gmail.com>
2019-03-09 00:44:30 +0900

    Add compose function

--- collatz.py
+++ collatz.py
@@ -1,3 +1,6 @@
+fron functools import reduce
+
+
 def even(n):
     return n % 2 == 0
 
@@ -13,11 +16,23 @@ def collatz(n):
         return [n] + collatz(n // 2)
     elif odd(n):
         return [n] + collatz(n*3 + 1)
-    
+
+
 def compose2(f, g):
     return lambda x: f(g(x))
 
 
+def compose(*functions):
+    
+    def id(x):
+        return x
+    
+    return reduce(compose2, functions, id)
+
+
 if __name__ == '__main__':
-    n = int(input())
-    print(collatz(n))
+    input_int = compose(int, input)
+    collatz_print = compose(print, collatz)
+    
+    n = input_int('')
+    collatz_print(n)
